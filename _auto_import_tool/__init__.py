027d6e19cd950c5a2ba226da129b389b897bbfd4
BlakeXYZ <blakekostroun@yahoo.com>
2023-10-31 23:08:41 -0500

    Update __init__.py

--- _auto_import_tool/__init__.py
+++ _auto_import_tool/__init__.py
@@ -1,25 +1,12 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 bl_info = {
-    "name" : "test",
-    "author" : "bk",
-    "description" : "",
-    "blender" : (2, 80, 0),
-    "version" : (0, 0, 1),
-    "location" : "",
+    "name" : "Auto Import Tool",
+    "author" : "Blake Kostroun",
+    "version" : (1, 0),
+    "blender" : (3, 2),
+    "location" : "View 3D > Tool",
     "warning" : "",
-    "category" : "Generic"
+    "wiki" : "https://www.artstation.com/blakekostroun",
+    "category" : "New Addon",
 }
 
 def register():
